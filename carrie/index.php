5f8bdd4a7ab6f8dd482c72fa3313d5a1f4692800
Christine McClure <camcclure@gmail.com>
2015-06-02 11:51:40 -0500

    new structure using php and folders

---
+++ carrie/index.php
@@ -0,0 +1,20 @@
+<?php include("..\includes\head.html");?>
+
+<div class="row">
+    <a name="carrie"></a><h2 class="name-header">Carrie's Picks</h2>
+    <div class="list-group">
+        
+        <div class="list-group-item">
+            <a target="_blank" href=""><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href=""><img src="../images/worldcat.jpg" /></a>
+        </div>
+        
+        <div class="list-group-item">
+            <a href="#top"><p class="btn top">top</p></a>
+        </div>
+        
+    </div>
+  </div>   
+
+</body>
+</html>
