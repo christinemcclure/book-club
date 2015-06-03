99db2677dd4451952a24a87d05c4b07e680e843b
Christine McClure <camcclure@gmail.com>
2015-06-03 07:43:49 -0500

    added head to index directly

--- index.php
+++ index.php
@@ -1,6 +1,47 @@
-<?php include("includes\head.html");?>
+<!DOCTYPE html>
+<html lang="en" id="christinemcclure-com"> 
+<head>
+  <meta name="description" content="">
+  <meta name="author" content="Christine McClure">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;" name="viewport" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <title>Book Club Selections</title>
+  <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700,900' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" media="screen" href="../stylesheets/screen.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="../stylesheets/css3.css" />
+  <link rel="stylesheet" type="text/css" media="print" href="../stylesheets/print.css" />
+ <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+ 
+  </head>
+  <body>
+    <a name="top"></a>
+<div class="container-fluid well well-lg">
+    <div class="row">
+        <h1><a href="\index.php">Book Club Picks</a></h1>
+  </div>
+    <div class="row">
+      <nav>
+        <a href="/carrie"><p class="btn btn-custom">Carrie</p></a>
+        <a href="/christine"><p class="btn btn-custom">Christine</p></a>
+        <a href="/elizabeth"><p class="btn btn-custom">Elizabeth</p></a>
+        <a href="/gretchen"><p class="btn btn-custom">Gretchen</p></a>
+        <a href="/jeff"><p class="btn btn-custom">Jeff</p></a>
+        <a href="/kate"><p class="btn btn-custom">Kate</p></a>
+        <a href="/lisa"><p class="btn btn-custom">Lisa</p></a>
+        <a href="/liz"><p class="btn btn-custom">Liz</p></a>
+        <a href="/pat"><p class="btn btn-custom">Pat</p></a>
+        <br/>
+        <a href="/movies"><p class="extra btn btn-custom">Movies!</p></a>
+        <a href="/cookbooks"><p class="extra btn btn-custom">Cookbooks!</p></a>
+        <br>
+<!--        <a target="_blank" href="https://www.surveymonkey.com/s/7QH3FDV"><p class="survey btn btn-custom">Survey</p></a>-->
+        <a href="/2014"><p class="survey btn btn-custom">View 2014</p></a>
+   </nav>
+    </div>         
 
-    
+  
   <div class="row">
     <div class="list-group">
       <h2 class="name-header">2015 Voting Round Open</h2>
