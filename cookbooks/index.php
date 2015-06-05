9917d432a9587a34c37bcf2b1f42ab052876148a
Christine McClure <camcclure@gmail.com>
2015-06-05 17:34:27 -0500

    added my pick. fixed paths

--- cookbooks/index.php
+++ cookbooks/index.php
@@ -3,13 +3,26 @@
   <div class="row">
    <h2 class="name-header">Cookbooks!</h2>
     <div class="list-group">
+    
+        <div class="list-group-item">
+            <h3>At Home with the French Classics</h3>
+            <h4>by Richard Grausman</h4>
+<p>Despite our growing kitchen savvy, many Americans still find French food too intimidating, too time-consuming, and too complicated. Richard Grausman, a well-known teacher of French cooking, has had a long-time mission to dispel this fear and make French cooking accessible and understandable to the American home cook. The result? AT HOME WITH THE FRENCH CLASSICS.</p>
+<p>This compendium of over 250 classic and traditional French dishes offers recipes that are not only approachable, but are also geared to the contemporary American lifestyle. The author has cut back on salt, fat, eggs, butter, and cream without any loss of authenticity or taste. The recipes have been streamlined by the use of modern appliances. Certain classic techniques, which are neither sensible for the modern kitchen nor vital for the recipe's success, have been revised or eliminated.</p>
+<p>Mediterranean Fish Soup. Roquefort Cheese Souffle. Zucchini stuffed with Mushrooms and Ham. Broiled Baby Chickens with Herbs and Mustard. Poached Chicken with Creamy Tarragon Sauce. Pepper-Coated Steak with Cognac and Cream. Rack of Lamb with Watercress. And for dessert, Pears Poached in Port Wine, Chocolate Mousse, and Rich Vanilla and Chocolate Custards.</p>
+<p>Starting with first courses and ending with after-dinner chocolates, the book contains dozens of possibilities for entertaining. Many of the dishes can be made ahead of time. Others can be made in stages, from days to weeks in advance. Selection of Better Homes & Gardens Family Book Service and Book-of-the-Month Cooking & Crafts Club. 82,000 copies in print.</p>
+
+            <a target="_blank" href="http://www.amazon.com/Home-French-Classics-Richard-Grausman/dp/0894806335/ref=sr_1_1?s=books&ie=UTF8&qid=1433543006&sr=1-1&keywords=at+home+with+the+french+classics+cookbook"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/at-home-with-the-french-classics/oclc/18135368&referer=brief_results" /><img src="../images/worldcat.jpg" /></a>
+        </div>     
+        
         
         <div class="list-group-item">
             <h3>A Boat, a Whale &amp; a Walrus: Menus and Stories</h3>
             <h4>by Renee Erickson</h4>
             <p>One of the country's most acclaimed chefs, Renee Erickson is a James-Beard nominated chef and the owner of several Seattle restaurants: The Whale Wins, Boat Street Café, The Walrus and the Carpenter, and Barnacle. This luscious cookbook is perfect for anyone who loves the fresh seasonal food of the Pacific Northwest. Defined by the bounty of the Puget Sound region, as well as by French cuisine, this cookbook is filled with seasonal, personal menus like Renee’s Fourth of July Crab Feast, Wild Foods Dinner, and a fall pickling party. Home cooks will cherish Erickson’s simple yet elegant recipes such as Roasted Chicken with Fried Capers and Preserved Lemons, Harissa-Rubbed Roasted Lamb, and Molasses Spice Cake. Renee Erickson's food, casual style, and appreciation of simple beauty is an inspiration to readers and eaters in the Pacific Northwest and beyond.</p>            
-            <a target="_blank" href="http://www.amazon.com/Boat-Whale-Walrus-Menus-Stories/dp/1570619263/ref=sr_1_1?s=books&ie=UTF8&qid=1433262871&sr=1-1&keywords=A+Boat%2C+A+Whale%2C+and+a+Walrus%2C+by+Renee+Erikson+and+Jess+Thomson"><img src="images/amazon.jpg" ></a>
-            <a target="_blank" href="http://www.worldcat.org/title/boat-a-whale-a-walrus-menus-and-stories/oclc/883365158&referer=brief_results"><img src="images/worldcat.jpg" /></a>
+            <a target="_blank" href="http://www.amazon.com/Boat-Whale-Walrus-Menus-Stories/dp/1570619263/ref=sr_1_1?s=books&ie=UTF8&qid=1433262871&sr=1-1&keywords=A+Boat%2C+A+Whale%2C+and+a+Walrus%2C+by+Renee+Erikson+and+Jess+Thomson"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/boat-a-whale-a-walrus-menus-and-stories/oclc/883365158&referer=brief_results"><img src="../images/worldcat.jpg" /></a>
         </div>        
 
         <div class="list-group-item">
@@ -17,8 +30,8 @@
             <h4>by Adrian Miller</h4>
             <p>In this insightful and eclectic history, Adrian Miller delves into the influences, ingredients, and innovations that make up the soul food tradition. Focusing each chapter on the culinary and social history of one dish--such as fried chicken, chitlins, yams, greens, and "red drinks--Miller uncovers how it got on the soul food plate and what it means for African American culture and identity.</p>
             <p>Miller argues that the story is more complex and surprising than commonly thought. Four centuries in the making, and fusing European, Native American, and West African cuisines, soul food--in all its fried, pork-infused, and sugary glory--is but one aspect of African American culinary heritage. Miller discusses how soul food has become incorporated into American culture and explores its connections to identity politics, bad health raps, and healthier alternatives. This refreshing look at one of America's most celebrated, mythologized, and maligned cuisines is enriched by spirited sidebars, photographs, and 22 recipes.</p>
-            <a target="_blank" href="http://www.amazon.com/Soul-Food-Surprising-American-Cuisine/dp/146960762X/ref=sr_1_1?s=books&ie=UTF8&qid=1433263070&sr=1-1&keywords=9781469607627"><img src="images/amazon.jpg" ></a>
-            <a target="_blank" href="http://www.worldcat.org/title/soul-food-the-surprising-story-of-an-american-cuisine-one-plate-at-a-time/oclc/836261092&referer=brief_results"><img src="images/worldcat.jpg" /></a>
+            <a target="_blank" href="http://www.amazon.com/Soul-Food-Surprising-American-Cuisine/dp/146960762X/ref=sr_1_1?s=books&ie=UTF8&qid=1433263070&sr=1-1&keywords=9781469607627"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/soul-food-the-surprising-story-of-an-american-cuisine-one-plate-at-a-time/oclc/836261092&referer=brief_results"><img src="../images/worldcat.jpg" /></a>
         </div>
         
        <div class="list-group-item">
