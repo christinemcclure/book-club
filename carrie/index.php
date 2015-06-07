97e6e2640ca0b1865a3a3f2d19f478fd614efacd
Christine McClure <camcclure@gmail.com>
2015-06-06 19:33:41 -0500

    added carries books

--- carrie/index.php
+++ carrie/index.php
@@ -5,11 +5,35 @@
     <div class="list-group">
         
         <div class="list-group-item">
-            <p>No entries yet. Boo!</p>
-            <a target="_blank" href=""><img src="../images/amazon.jpg" ></a>
-            <a target="_blank" href=""><img src="../images/worldcat.jpg" /></a>
+          <h3>Cutting the Stone</h3>
+          <h4>by Abraham Verghese</h4>
+            <p>Marion and Shiva Stone are born one dramatic afternoon in 1954 in Addis Ababa, the same day their mother — a nun, Sister Mary Joseph Praise — dies of complications from her hidden pregnancy. The boys are conjoined at the skull, yet separated at birth; they are raised by Dr. Kalpana Hemlatha, a forceful woman known as Hema, and Dr. Abhi Ghosh, both immigrants from Madras and both doctors at the hospital where the boys’ natural parents also worked. They grow up amid the political turmoil of Ethiopia, and in 1979 Marion flees, first to Nairobi and finally to New York, where he qualifies as a surgeon. Shiva, too, goes into medicine, specializing in treating vaginal fistula, for which work he is acclaimed. Almost supernaturally close as children, the brothers become more and more distant as the novel progresses; they are dramatically reunited at its end — through the mysterious agency of the long-vanished Thomas Stone.</p>
+            <a target="_blank" href="http://www.amazon.com/Cutting-Stone-Abraham-Verghese/dp/0375714367/ref=sr_1_1?s=books&ie=UTF8&qid=1433621793&sr=1-1&keywords=cutting+the+stone"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/cutting-for-stone-a-novel/oclc/502676860&referer=brief_results"><img src="../images/worldcat.jpg" /></a>
         </div>
-        
+
+        <div class="list-group-item">
+          <h3>Stealing All Transmissions</h3>
+          <h4>by Randal Doane</h4>
+            <p>"Diane documents with wit and verve how The Clash leapt from the Westway to the USA in 1979 with the help of rock journalists and key deejays in the States. It was an honor to work with The Clash back in the day, and a pleasure revisiting those days through this book. Doane’s passion for The Clash and their comrades resonates on every page of this dynamic read. Stealing’s a must have for Clashophiles on both sides of the Atlantic." —Dan Beck, former senior vice president at Epic Records, managing partner, Big Honcho Media.
+            </p>
+            <p>
+"Stealing is a must-read for music fans of all varieties, for it’s much more than a book about The Clash. With a captivating narrative and well-written prose, Stealing makes sense of what happened to free-form radio and the DIY ethic of punk, and deftly connects that history to the era of file-sharing and satellite radio. Don’t miss this book." —Michael Roberts, author, Tell Tchaikovsky the News: Rock ’n’ Roll, the Labor Question, and the Musicians’ Union, 1942–1968
+</p>
+            <a target="_blank" href="http://www.amazon.com/Stealing-All-Transmissions-Secret-History/dp/1629630292/ref=sr_1_1?s=books&ie=UTF8&qid=1433637019&sr=1-1&keywords=Stealing+All+Transmissions%2C+Randal+Doane"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/stealing-all-transmissions-a-secret-history-of-the-clash/oclc/874099451&referer=brief_results"><img src="../images/worldcat.jpg" /></a>
+        </div>      
+        <div class="list-group-item">
+          <h3>When Books Went to War: The Stories that Helped Us Win World War II</h3>
+          <h4>by Molly Guptill Manning</h4>
+            <p>When America entered World War II in 1941, we faced an enemy that had banned and burned over 100 million books and caused fearful citizens to hide or destroy many more. Outraged librarians launched a campaign to send free books to American troops and gathered 20 million hardcover donations. In 1943, the War Department and the publishing industry stepped in with an extraordinary program: 120 million small, lightweight paperbacks, for troops to carry in their pockets and their rucksacks, in every theater of war.
+            </p>
+            <p>
+Comprising 1,200 different titles of every imaginable type, these paperbacks were beloved by the troops and are still fondly remembered today. Soldiers read them while waiting to land at Normandy; in hellish trenches in the midst of battles in the Pacific; in field hospitals; and on long bombing flights. They wrote to the authors, many of whom responded to every letter. They helped rescue The Great Gatsby from obscurity. They made Betty Smith, author of A Tree Grows in Brooklyn, into a national icon. When Books Went to War is an inspiring story for history buffs and book lovers alike.
+</p>
+            <a target="_blank" href="http://www.amazon.com/When-Books-Went-War-Stories/dp/0544535022/ref=sr_1_1?s=books&ie=UTF8&qid=1433637146&sr=1-1&keywords=When+Books+Went+to+War%3A+The+Stories+that+Helped+Us+Win+World+War+II%2C+Molly+Guptill+Manning"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/when-books-went-to-war-the-stories-that-helped-us-win-world-war-ii/oclc/884299002&referer=brief_results"><img src="../images/worldcat.jpg" /></a>
+        </div>         
         <div class="list-group-item">
             <a href="#top"><p class="btn top">top</p></a>
         </div>
