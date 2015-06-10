da141536f8f579ce2203aaf44be66a7ca1234444
Christine McClure <camcclure@gmail.com>
2015-06-09 19:44:19 -0500

    added elizabeth's book picks

--- elizabeth/index.php
+++ elizabeth/index.php
@@ -5,12 +5,45 @@
     <div class="list-group">
         
         <div class="list-group-item">
-            <p>No entries yet. Boo!</p>
-            <a target="_blank" href=""><img src="../images/amazon.jpg" ></a>
-            <a target="_blank" href=""><img src="../images/worldcat.jpg" /></a>
+          <h3>The Handmaid's Tale</h3>
+          <h4>Margaret Atwood</h4>
+          <p>In the world of the near future, who will control women's bodies?</p>
+          <p>
+            Offred is a Handmaid in the Republic of Gilead. She may leave the home of the Commander and his wife once a day to walk to food markets whose signs are now pictures instead of words because women are no longer allowed to read. She must lie on her back once a month and pray that the Commander makes her pregnant, because in an age of declining births, Offred and the other Handmaids are only valued if their ovaries are viable.</p>
+          <p>
+
+            Offred can remember the days before, when she lived and made love with her husband Luke; when she played with and protected her daughter; when she had a job, money of her own, and access to knowledge. But all of that is gone now....</p>
+          <p>
+
+            Funny, unexpected, horrifying, and altogether convincing, The Handmaid's Tale is at once scathing satire, dire warning, and tour de force.</p>
+            <a target="_blank" href="http://www.worldcat.org/title/infinite-jest-a-novel/oclc/493204290&referer=brief_results"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/handmaids-tale/oclc/12558693&referer=brief_results"><img src="../images/worldcat.jpg" /></a>
         </div>
         
+        
         <div class="list-group-item">
+          <h3>The House of the Spirits: A Novel</h3>
+          <h4>by Isabel Allende</h4>
+          <p>In one of the most important and beloved Latin American works of the twentieth century, Isabel Allende weaves a luminous tapestry of three generations of the Trueba family, revealing both triumphs and tragedies. Here is patriarch Esteban, whose wild desires and political machinations are tempered only by his love for his ethereal wife, Clara, a woman touched by an otherworldly hand. Their daughter, Blanca, whose forbidden love for a man Esteban has deemed unworthy infuriates her father, yet will produce his greatest joy: his granddaughter Alba, a beautiful, ambitious girl who will lead the family and their country into a revolutionary future.</p>
+          <p>
+            The House of the Spirits is an enthralling saga that spans decades and lives, twining the personal and the political into an epic novel of love, magic, and fate.</p>
+            <a target="_blank" href="http://www.amazon.com/The-House-Spirits-A-Novel/dp/0553383809"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/house-of-the-spirits/oclc/61493264&referer=brief_results"><img src="../images/worldcat.jpg" /></a>
+        </div>
+
+        
+        <div class="list-group-item">
+          <h3>Infinite Jest</h3>
+          <h4>by David Foster Wallace </h4>
+          <p>A gargantuan, mind-altering comedy about the pursuit of happiness in America. Set in an addicts' halfway house and a tennis academy, and featuring the most endearingly screwed-up family to come along in recent fiction, Infinite Jest explores essential questions about what entertainment is and why it has come to so dominate our lives; about how our desire for entertainment affects our need to connect with other people; and about what the pleasures we choose say about who we are. </p>
+          <p>
+            Equal parts philosophical quest and screwball comedy, Infinite Jest bends every rule of fiction without sacrificing for a moment its own entertainment value. It is an exuberant, uniquely American exploration of the passions that make us human - and one of those rare books that renew the idea of what a novel can do.</p>
+            <a target="_blank" href="http://www.amazon.com/Infinite-Jest-David-Foster-Wallace/dp/0316066524"><img src="../images/amazon.jpg" ></a>
+            <a target="_blank" href="http://www.worldcat.org/title/infinite-jest-a-novel/oclc/493204290&referer=brief_results"><img src="../images/worldcat.jpg" /></a>
+        </div>
+
+      
+      <div class="list-group-item">
             <a href="#top"><p class="btn top">top</p></a>
         </div>
         
